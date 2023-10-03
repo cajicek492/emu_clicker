7750a4ee52b4a9666742169c1ee7f1038806155b
filiphubler <filip.hubler@student.spsmb.cz>
2023-10-03 19:18:33 +0200

    click_funkce

--- res/js/main.js
+++ res/js/main.js
@@ -1,1 +1,10 @@
-const Emu = document.getElementById(Emu);
+const emu = document.getElementById("emu");
+const counter = document.getElementById("counter");
+
+let numberOfEmus = 0;
+
+emu.onclick = () => {
+    console.log("click");
+    numberOfEmus ++;
+    counter.innerText = "Emus: " + numberOfEmus;
+}
