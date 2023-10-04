842376a486a71aca563b6f97b5fda1b58e08b26e
filiphubler <filip.hubler@student.spsmb.cz>
2023-10-04 17:28:51 +0200

    vzhled_autoclicker

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,47 @@
 const emu = document.getElementById("emu");
 const counter = document.getElementById("counter");
+const basicEmu = document.getElementById("basicEmu");
+const advancedEmu = document.getElementById("advancedEmu");
 
 let numberOfEmus = 0;
+let emusPerClick = 1;
+let emuAutoClick = 0;
+let emuAutoInterval;
+let costOfBasicUpgrade = 20;
+let costOfAdvacedUpgrade = 100;
 
 emu.onclick = () => {
     console.log("click");
-    numberOfEmus ++;
+    numberOfEmus += emusPerClick;
     counter.innerText = "Emus: " + numberOfEmus;
 }
+
+
+basicEmu.onclick = () => {
+    if(numberOfEmus >= costOfBasicUpgrade){
+        console.log("upgrade bought");
+        numberOfEmus -= costOfBasicUpgrade;
+        emusPerClick++;
+        counter.innerText = "Emus: " + numberOfEmus;
+    }
+}
+
+advancedEmu.onclick = () => {
+    if(numberOfEmus >= costOfAdvacedUpgrade){
+        console.log("it works maybe");
+        numberOfEmus -= costOfAdvacedUpgrade;
+        counter.innerText = "Emus: " + numberOfEmus;
+        emuAutoClick += 1;
+        clearInterval(emuAutoInterval);
+        emuAutoInterval = setInterval(() => {
+            numberOfEmus += emuAutoClick;
+            counter.innerText = "Emus: " + numberOfEmus;
+        },1000);
+    }
+}
+
+function hackerMan () {
+    numberOfEmus += 10000;
+    counter.innerText = "Emus: " + numberOfEmus;
+}
+hackerMan();
