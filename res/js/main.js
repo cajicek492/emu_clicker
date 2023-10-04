10cc444ccec7650b5a8c8c4db60b2269a12d8406
filiphubler <filip.hubler@student.spsmb.cz>
2023-10-04 20:43:55 +0200

    superior autoclicker

--- res/js/main.js
+++ res/js/main.js
@@ -1,15 +1,19 @@
 const emu = document.getElementById("emu"); // emu na click
 const counter = document.getElementById("counter");
 const basicEmu = document.getElementById("basicEmu"); // upgrade obrazek
-const advancedEmu = document.getElementById("advancedEmu");
+const advancedEmu = document.getElementById("advancedEmu"); // upgrade obrazek
+const superiorEmu = document.getElementById("superiorEmu");
 const priceCounterBasic = document.getElementById("priceCounterBasic");
 
 let numberOfEmus = 0;
 let emusPerClick = 1;
 let emuAutoClick = 0;
+let emuSuperiorAutoClick = 0;
 let emuAutoInterval;
+let emuSuperiorAutoInterval;
 let costOfBasicUpgrade = 20; // basic upgrade je na click
 let costOfAdvacedUpgrade = 100;
+let costOfSuperiorUpgrade = 1000;
 
 
 emu.onclick = () => {
@@ -44,6 +48,19 @@ advancedEmu.onclick = () => {
     }
 }
 
+superiorEmu.onclick = () => {
+    if(numberOfEmus >= costOfSuperiorUpgrade){
+        console.log("budeme na prime");
+        numberOfEmus -= costOfSuperiorUpgrade;
+        counter.innerText = "Emus: " + numberOfEmus;
+        emuSuperiorAutoClick += 10;
+
+        emuSuperiorAutoInterval =setInterval(() => {
+            numberOfEmus += emuSuperiorAutoClick;
+            counter.innerText = "Emus: " + numberOfEmus;
+        },1000)
+    }
+}
 function hackerMan () {
     numberOfEmus += 10000;
     counter.innerText = "Emus: " + numberOfEmus;
